088a24727f80d3e4a040509e7e74991e3151e65b
Ajay Kumar Shakya <ajayshakya7376@gmail.com>
2023-12-04 23:14:12 +0530

    adding some files of backend to the server

--- 2.express/index.js
+++ 2.express/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 4000;
-
+//  ConnectDb();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); 
